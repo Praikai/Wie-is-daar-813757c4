eaf83583855475ec97e48aaf76e60ab6b56e97ce
Portfolio_Arsh Butt <45206255+Praikai@users.noreply.github.com>
2020-05-13 02:09:58 +0200

    Add files via upload

--- groet.php
+++ groet.php
@@ -1,42 +1,35 @@
 <!DOCTYPE html>
 <html>
-
-    <head>
-
-
-    </head>    
-
-    <style>
-    
-        #groet{
-            font-family: cursive;
-        }
-    
-    </style>
+s
+<style>
+    #groet {
+        font-family: cursive;
+    }
+</style>
 
 <body>
     <p id="groet">Hallo, wat is jouw naam?</p>
-    
-  <form method="post">
-    
-    <input type="text" name="naam" required>
-    <input type="submit" value="verzend" name="groet">
-    
-    
-  </form>
-        
-    
-  <?php
+
+    <form method="post">
+
+        <input type="text" name="naam" required>
+        <input type="submit" value="verzend" name="groet">
+
+
+    </form>
+
+
+    <?php
         if(isset($_POST['naam']))
         {
             echo "<br>Hallo, " . $_POST['naam'];
         }
 
- ?>  
-    
-    
-    
-    
+ ?>
+
+
+
+
 </body>
 
 
