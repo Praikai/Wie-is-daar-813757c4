402fa2423370a95fb415277760698062ecfb8678
Portfolio_Arsh Butt <45206255+Praikai@users.noreply.github.com>
2020-05-13 02:26:15 +0200

    Add files via upload

--- groet.php
+++ groet.php
@@ -1,10 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    
+        echo "Hallo gebruiker!" ;
+    
+    ?>
+    
+    <br>
+    <br>
+    
+    <form method="post">
+
+        <label for="naam">Wat is je naam?</label>
+        <input type="text" name="naam" required><br>
+
+        <input type="submit" value="verzend" name="groet">
+
+
+    </form>
+
+
+    <?php
+        if(isset($_POST['naam']))
+        {
+            echo "Hallo, " . $_POST['naam'];
+        }
+
+ ?>
 
 
 
-echo str_replace("gebruiker", "computer", "Hallo gebruiker!");
+
+</body>
 
 
 
 
-?>
+
+
+
+
+
+</html>
