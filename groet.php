a2f778f15b5181e4bea2e3a6d2b1dfeba2ec51e8
Portfolio_Arsh Butt <45206255+Praikai@users.noreply.github.com>
2020-05-28 12:44:47 +0200

    Add files via upload

--- groet.php
+++ groet.php
@@ -1,47 +1,12 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<body>
+//readline 
+   $groet = readline('hoi gebruiker! '); 
+  
+// For output 
 
-    <?php
-    
-        echo "Hallo gebruiker!" ;
-    
-    ?>
-    
-    <br>
-    <br>
-    
-    <form method="post">
+    echo $groet;     
 
-        <label for="naam">Wat is je naam?</label>
-        <input type="text" name="naam" required><br>
-
-        <input type="submit" value="verzend" name="groet">
+?> 
 
 
-    </form>
-
-
-    <?php
-        if(isset($_POST['naam']))
-        {
-            echo "Hallo, " . $_POST['naam'];
-        }
-
- ?>
-
-
-
-
-</body>
-
-
-
-
-
-
-
-
-
-</html>
